0cbf233ee4585698dac07ccf1c237da09a238214
adityagaikwad888 <adityakg99@gmail.com>
2025-06-18 17:29:43 +0530

    updated clone public repo

--- vars/ClonePublicRepo.groovy
+++ vars/ClonePublicRepo.groovy
@@ -1,4 +1,8 @@
-def call(String repoUrl, String targetBranch) {
-    // sh "git clone -b ${targetBranch} --single-branch ${repoUrl} ."
-    git url: repoUrl, branch: targetBranch
+// In your shared library
+def ClonePublicRepo(String url, String branch) {
+    if (isUnix()) {
+        sh "git clone -b ${branch} ${url} ."
+    } else {
+        bat "git clone -b ${branch} ${url} ."
+    }
 }
